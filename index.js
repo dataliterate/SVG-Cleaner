4274d5f052723d14f30a605d79593be6642cafa1
rockitbaby <babe@rockitbaby.de>
2012-08-15 15:08:23 +0200

    implemented repairStyles, simplified tests

--- index.js
+++ index.js
@@ -103,50 +103,243 @@ function removeNamespacedAttributes(namespacesToRemove) {
   });
 
 }
-
+var i = 0;
 function removeComments() {
 
+  var commentNodes = [];
+
   // process on NODE level
-  function searchAndRemoveComment(root) {
-    _.each(root.children, function(node) {
-      if(node.type == 'comment') {
-        $(node).remove();
-      }
-      if(node.type == 'tag') {
-        // walk the NODE tree
-        _.each(node.children, searchAndRemoveComment);
-      }
-    });
+  function searchForComment(node) {
+    if(node.type == 'comment') {
+      commentNodes.push(node);
+    }
+    if(!_(node.children).isUndefined()) {
+      _.each(node.children, searchForComment);
+    }
   }
-  searchAndRemoveComment($._root);
+  searchForComment($._root);
+  _.each(commentNodes, function(node) {
+    $(node).remove();
+  });
 }
 
+function mayContainTextNode(node) {
+
+  var result = true;
+
+  var elementsThatDontContainText = ['rect', 'circle', 'ellipse', 'line', 'polygon', 'polyline', 'path', 'image', 'stop'];
+  var elementsThatCouldContainText = ['g', 'clipPath', 'marker', 'mask', 'pattern', 'linearGradient', 'radialGradient', 'symbol'];
+
+  if(node.type == 'comment') {
+    result = false;
+  }
+  else if(_(elementsThatDontContainText).include(node.name)) {
+    result = false;
+  }
+  else if(_(elementsThatCouldContainText).include(node.name)) {
+    result = false;
+    if(!_(node.children).isUndefined() && node.children.length) {
+      result = _(node.children).any(mayContainTextNode);
+    }
+  }
+
+  return result;
+
+}
+
+function repairStyles() {
+  $('*').each(function(ix, node) {
+    repairStyle($(node));
+  });
+}
+
+function repairStyle(node) {
+
+  var $node = node;
+
+  var styles = parseStyles($node.attr('style'));
+
+  if(!_(styles).size()) {
+    return;
+  }
+
+  // I've seen this enough to know that I need to correct it:
+  // fill: url(#linearGradient4918) rgb(0, 0, 0);
+  _.each(['fill', 'stroke'], function(property) {
+    if(!_(styles).has(property)) {
+      return;
+    }
+    var chunk = styles[property].split(') ');
+    if (chunk.length == 2 && chunk[0].substr(0, 5) == 'url(#' || chunk[0].substr(0, 6) == 'url("#' || chunk[0].substr(0, 6) == "url('#" && chunk[1] == 'rgb(0, 0, 0)') {
+      styles[property] = chunk[0] + ')'
+    }
+  });
+
+  var STYLE_PROPERTIES = [];
+  STYLE_PROPERTIES['stroke'] = ['stroke', 'stroke-width', 'stroke-linejoin', 'stroke-opacity', 'stroke-miterlimit', 'stroke-linecap', 'stroke-dasharray', 'stroke-dashoffset', 'stroke-opacity'];
+  STYLE_PROPERTIES['fill'] = ['fill', 'fill-opacity', 'fill-rule'];
+  STYLE_PROPERTIES['text'] = [ 'font-family', 'font-size', 'font-stretch', 'font-size-adjust', 'font-style', 'font-variant', 'font-weight', 'letter-spacing', 'line-height', 'kerning', 'text-align', 'text-anchor', 'text-decoration', 'text-rendering', 'unicode-bidi', 'word-spacing', 'writing-mode'];
+
+  var VENDOR_PREFIXES = ['-inkscape'];
+
+  var SVG_ATTRIBUTES = ['clip-rule', 'display', 'fill', 'fill-opacity', 'fill-rule', 'filter', 'font-family', 'font-size', 'font-stretch', 'font-style', 'font-variant', 'font-weight', 'line-height', 'marker', 'marker-end', 'marker-mid', 'marker-start', 'opacity', 'overflow', 'stop-color', 'stop-opacity', 'stroke', 'stroke-dasharray', 'stroke-dashoffset', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'visibility'];
+
+  if(_(styles).has('opacity')) {
+    var opacity = parseFloat(styles['opacity']);
+    if(opacity != 0.0) {
+      return;
+    }
+    // if opacity='0' then all fill and stroke properties are useless, remove them
+    styles = removeStyleProperties(styles, _.union(STYLE_PROPERTIES['stroke'], STYLE_PROPERTIES['fill']));
+
+  }
+  
+  // if stroke:none, then remove all stroke-related properties (stroke-width, etc)
+  if(_(styles).has('stroke') && styles['stroke'] == 'none') {
+    _(STYLE_PROPERTIES['stroke']).without('stroke');
+    styles = removeStyleProperties(styles, _(STYLE_PROPERTIES['stroke']).without('stroke'));
+  }
+
+  // if fill:none, then remove all fill-related properties (fill-rule, etc)
+  if(_(styles).has('fill') && styles['fill'] == 'none') {
+    styles = removeStyleProperties(styles, _(STYLE_PROPERTIES['fill']).without('fill'));
+  }
+
+  if(_(styles).has('fill-opacity') && parseFloat(styles['fill-opacity']) == 0.0) {
+    styles = removeStyleProperties(styles, _(STYLE_PROPERTIES['fill']).without('fill-opacity'));
+  }
+
+  if(_(styles).has('stroke-opacity') && parseFloat(styles['stroke-opacity']) == 0.0) {
+    styles = removeStyleProperties(styles, _(STYLE_PROPERTIES['stroke']).without('stroke-opacity'));
+  }
+
+  if(_(styles).has('stroke-width') && styleValue(styles['stroke-width']) == 0.0) {
+    styles = removeStyleProperties(styles, _(STYLE_PROPERTIES['stroke']).without('stroke-width'));
+  }
+
+  // remove all text styles, if node does not contain text
+  if(!mayContainTextNode(node[0])) {
+    styles = removeStyleProperties(styles, STYLE_PROPERTIES['text']);
+  }
+
+  // remove style with vendor prefixes
+  var stylesWithVendorPrefixes = _(styles).keys().filter(function(key) {
+    return _(VENDOR_PREFIXES).any(function(prefix) {
+      return (key.indexOf(prefix) == 0);
+    });
+  });
+  styles = removeStyleProperties(styles, stylesWithVendorPrefixes);
+
+  if(options.styleToAttributes) {
+    _(styles).each(function(value, key) {
+      if(!_(SVG_ATTRIBUTES).include(key)) {
+        return;
+      }
+      $(node).attr(key, value);
+      delete styles[key];
+    });
+  }
+  $node.attr('style', renderStyles(styles));
+
+}
+
+function styleValue(string) {
+  return parseFloat(string.replace(/[^-\d\.]/g, ''));
+}
+
+function removeStyleProperties(styles, properties) {
+  _(styles).each(function(value, key) {
+    if(_(properties).include(key)) {
+      delete styles[key]
+    }
+  });
+  return styles;
+}
+
+function parseStyles(inlineStyles) {
+  // now get all style properties
+
+  var styles = {};
+
+  if(_.isUndefined(inlineStyles)) {
+    return styles;
+  }
+  var styleRules = inlineStyles.split(';');
+  _(styleRules).each(function(style) {
+    var keyValue = style.split(':');
+    if(keyValue.length == 2) {
+      styles[keyValue[0].replace(/[\s]/g, '')] = keyValue[1];
+    }
+  });
+  return styles;
+}
+
+function renderStyles(styles) {
+  // 
+  if(!_(styles).size()) {
+    return '';
+  }
+
+  var inlineStyles = '';
+  _.each(styles, function(property, value) {
+    inlineStyles += value + ':' + property + ';';
+  });
+  return inlineStyles;
+}
+
+
+var options = {
+  styleToAttributes: true
+}
+
+// INTERFACE
 function clean(svgString, options) {
   
-  //root = cheerio(svgString);
-  $ = cheerio.load(svgString);
+  var aSVGCleaner = new SVGCleaner(options);
+  aSVGCleaner.load(svgString);
+  aSVGCleaner.clean();
+  return aSVGCleaner.svg;
 
-  removeNamespacedElements(namespacePrefixes);
-  removeNamespacedAttributes(namespacePrefixes);
-  removeComments();
-  //var referenced = findReferencedElements();
-
-
-  //console.log(findElementsWithIdAttribute().size());
-  //console.log(referenced.length);
-  var svg = $.html(); //.html();
-  console.log(svg);
-  return svg;
 }
 
 function cleanFile(filename, options) {
   return clean(fs.readFileSync(filename, 'utf-8'));
 }
 
-//cleanFile('stack.svg');
+var defaultOptions = {
+  styleToAttributes: true
+}
+var options = {};
+
+function SVGCleaner(_options) {
+  _(options).extend(defaultOptions, _options);
+  return this;
+}
+
+SVGCleaner.prototype.load = function(svgString) {
+  this.$ = cheerio.load(svgString, { ignoreWhitespace: true, xmlMode: true });
+  this.svg = this.$.html();
+}
+
+SVGCleaner.prototype.clean = function() {
+  $ = this.$;
+  removeNamespacedElements(namespacePrefixes);
+  removeNamespacedAttributes(namespacePrefixes);
+  removeComments();
+  repairStyles();
+  this.$ = $;
+  this.svg = $.html();
+}
+
+//cleanFile('test/files/simple.svg');
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="fill:#000000;" /> \
+                </svg>';
+console.log(clean(svg));
 
 module.exports = {
-    clean: clean
-  , cleanFile: cleanFile
+    clean: clean // Static
+  , cleanFile: cleanFile // Static
+  , SVGCleaner: SVGCleaner
 }
 
--- test/test.js
+++ test/test.js
@@ -34,35 +34,106 @@ describe('SVGCleaner', function() {
 
     it('should remove namespace attributes', function() {
       var cleanedSVG = SVGCleaner.clean(simpleSvg);
-      console.log(cleanedSVG);
       expect(cheerio(cleanedSVG).find('[inkscape:label]')).to.have.length(0);
     });
   });
 
   describe('Removal of comments', function() {
 
-    function containsComment(node) {
-      var hasComment = false;
-      _.each(node.children, function(node) {
-        hasComment = hasComment || (node.type == 'comment');
-        if(node.type == 'tag') {
-          // walk the NODE tree
-          _.each(node.children, function(child) {
-            hasComment = hasComment || containsComment(child);
-          });
-        }
-      });
-      return hasComment;
-    }
-
-    it('should be tested against a file that contains a comment', function() {
-      expect(containsComment(cheerio.load(simpleSvg)._root)).to.be(true);
-    });
-
     it('should remove all comments', function() {
-      var cleanedSVG = SVGCleaner.clean(simpleSvg);
-      expect(containsComment(cheerio.load(cleanedSVG)._root)).to.be(false);
+
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><!-- comment --><svg> \
+                  <!-- comment --> \
+                  <g style="font-family: Arial;"> \
+                    <!-- comment --> \
+                  </g> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg);
+      expect(cleanedSVG).not.to.match(/<!-- comment -->/);
     });
 
   });
+
+  describe('Style Repair', function() {
+
+    it('should fix remove color definition when fill or stroke refrences an url', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="fill:url(#test) rgb(0,0,0); stroke:url(#test) rgb(0,0,0);" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.equal('fill:url(#test);stroke:url(#test);');
+    });
+
+    it('should remove stroke- and fill-styles, when opacity is 0', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="opacity:0;fill:#0000ff;fill-rule:evenodd;stroke:#000000;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.equal('opacity:0;');
+    });
+
+    it('should remove stroke- and fill-styles, when stroke or fill is none', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="fill:none;fill-rule:evenodd;stroke:none;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.equal('fill:none;stroke:none;');
+    });
+
+    it('should remove stroke- and fill-styles, when stroke- or fill-opacity is 0', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg>\
+                    <rect style="fill:#000000;fill-rule:evenodd;fill-opacity:0;stroke:#000000;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:0" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.equal('fill-opacity:0;stroke-opacity:0;');
+    });
+
+    it('should remove stroke-styles, when stroke-with is 0', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="stroke:#000000;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1;stroke-width:0" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.equal('stroke-width:0;');
+    });
+
+    it('should remove text-styles, if element does not contain text', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="font-family:Arial;font-style:bold" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.be(undefined);
+    });
+
+    it('should keep text-styles, if element could contain text', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                  <g style="font-family: Arial;"> \
+                    <text>Hello World</text> \
+                    <rect width="10" height="10" /> \
+                  </g> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('g').attr("style")).to.equal('font-family: Arial;');
+    });
+
+    it('should remove styles with vendor prefix', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="-inkscape-font-specification:tbd;" /> \
+                </svg>';
+      var cleanedSVG = SVGCleaner.clean(svg, {styleToAttributes: false});
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.be(undefined);
+    });
+
+    it('should convert styles to attributes', function() {
+      var svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg> \
+                    <rect style="fill:#000000;" /> \
+                </svg>';
+      console.log("CONVERT");
+      var cleanedSVG = SVGCleaner.clean(svg);
+      console.log(cleanedSVG);
+      expect(cheerio.load(cleanedSVG)('rect').attr("style")).to.be(undefined);
+      expect(cheerio.load(cleanedSVG)('rect').attr("fill")).to.equal('#000000');
+    });
+
+  });
+
 });
